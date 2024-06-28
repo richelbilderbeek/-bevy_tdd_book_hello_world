a2750eb69fb82b040ede4fdaf5745e16d637d1c2
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-06-28 12:33:05 +0200

    100% codecov

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
-use bevy::prelude::*;
-
-/// Create a Bevy app
-fn create_app() -> App {
-    return App::new();
-}
+use crate::app::create_app;
+mod app;
 
 fn main() {
     let mut app = create_app();
     app.run();
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_testing() {
-        assert_eq!(1 + 1, 2)
-    }
-
-    #[test]
-    fn test_can_create_app() {
-        create_app();
-    }
 }
